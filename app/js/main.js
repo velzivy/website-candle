f47e436eaa9eab3d6ebeffcd5d9d731f9536c63c
Fymrynight <fymrynight@gmail.com>
2023-10-05 17:41:28 +0300

    mainsection

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector('.menu__btn')
-
+const menuList = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', function() {
-	menuBtn.classList.toggle('menu__btn--open');
+	menuList.classList.toggle('menu__btn--open');
 })
